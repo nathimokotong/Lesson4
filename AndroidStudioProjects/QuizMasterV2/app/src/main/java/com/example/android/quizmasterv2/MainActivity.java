8a459cdc6d85ad53c6aa3427f2c3b86bbb098544
Isaiah <nathi92@ymail.com>
2016-10-11 14:47:54 +0200

    Quiz V2, with one question left and removed actionbar menu

--- AndroidStudioProjects/QuizMasterV2/app/src/main/java/com/example/android/quizmasterv2/MainActivity.java
+++ AndroidStudioProjects/QuizMasterV2/app/src/main/java/com/example/android/quizmasterv2/MainActivity.java
@@ -15,35 +15,18 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Dialog dialog;
-    Button button;
-    TextView sportscore;
-    TextView bioscore;
-    TextView historyscore;
-  public String score1 = "0";
-  public String score2 = "0";
-  public String score3 = "0";
-    int text = 0;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        dialog = new Dialog(MainActivity.this);
-
-        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
-        sportscore = (TextView) findViewById(R.id.sprortsscore);
-        bioscore = (TextView) findViewById(R.id.bioscore);
-        historyscore = (TextView) findViewById(R.id.historysscore);
-
     }
 
-
+/*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 
@@ -62,13 +45,14 @@ public class MainActivity extends AppCompatActivity {
         if(item.getItemId() == R.id.scoresid)
         {
 
-            dialog(score1,score2,score3);
+            Intent intent = new Intent(MainActivity.this,Marks.class);
+            startActivity(intent);
 
         }
 
 return true;
     }
-
+*/
     public void sports(View view)
 {
 
@@ -80,21 +64,13 @@ return true;
 
     public void biology(View view)
     {
-       // Intent intent = new Intent(MainActivity.this,Biology.class);
+       Intent intent = new Intent(MainActivity.this,Biology.class);
 
-       // startActivity(intent);
+        startActivity(intent);
     }
 
 
-    public void dialog(String sc1,String sc2,String sc3)
-    {
-        dialog.setContentView(R.layout.scores_layout);
 
-       sportscore.setText(sc1);
-        bioscore.setText(sc2);
-        historyscore.setText(sc3);
-        dialog.show();
-    }
 
 
 }
